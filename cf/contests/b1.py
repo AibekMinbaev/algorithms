9826face6ccd2e0232033c145a8140511a5b24da
Aibek Minbaev <aibekminbaev050402@gmail.com>
2025-03-06 17:44:47 +0900

    done: div 2 B

--- cf/contests/b1.py
+++ cf/contests/b1.py
@@ -6,84 +6,16 @@ for _ in range(t):
     mat = [] 
     for r in range(n): 
         col = input().split() 
-        mat.append(col)  
+        mat.append(col) 
     
-
-    d = {} 
-    for r in range(n): 
-        for c in range(m): 
-            col = mat[r][c] 
-            if col not in d: 
-                d[col] = []  
-            
-            for sets in d[col]: 
-                if (r,c) not in sets: 
-                    sets.add((r-1,c)) 
-                    sets.add((r+1, c)) 
-                    sets.add((r,c-1)) 
-                    sets.add((r,c+1)) 
-                    break 
-            else: 
-                st = set() 
-                st.add((r-1,c)) 
-                st.add((r+1, c)) 
-                st.add((r,c-1)) 
-                st.add((r,c+1))  
-                d[col].append(st) 
+    mx = 1 
+    d = {}  
+    for i in range(n): 
+        for j in range(m): 
+            if mat[i][j] not in d: 
+                d[mat[i][j]] = 1 
+            if (i+1 < n and mat[i][j] == mat[i+1][j]) or (j+1 < m and mat[i][j] == mat[i][j+1]): 
+                d[mat[i][j]] = 2 
+                mx = 2
     
-    res = 0 
-    mx = 0 
-    for k, v in d.items(): 
-        res += len(v) 
-        mx = max(mx, len(v)) 
-    print(res - mx) 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# st1 = set() 
-# st2 = set() 
-
-# for r in range(n): 
-#     for c in range(m):  
-#         if (r+c) % 2 == 0: 
-#             st1.add(mat[r][c]) 
-#         else: 
-#             st2.add(mat[r][c]) 
-
-# res = len(st1) + len(st2) 
-# mn_res = res
-
-# for elem in st1: 
-#     print(elem, res) 
-#     if elem in st2: 
-#         mn_res = min(mn_res, res - 2) 
-#     else: 
-#         mn_res = min(mn_res, res - 1) 
-
-# print(mn_res) 
-        
-
-
-
-
+    print(sum(d.values()) - mx) 
