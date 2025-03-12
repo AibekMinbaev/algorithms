e674e5138ee58571ab18458ab47a225d457c79b4
Aibek Minbaev <aibekminbaev050402@gmail.com>
2025-03-12 12:22:01 +0900

    upsolving div2 C

--- cf/contests/2078/c.py
+++ cf/contests/2078/c.py
@@ -3,22 +3,36 @@ t = int(input())
 for _ in range(t): 
     n = int(input()) 
     arr = list(map(int, input().split())) 
-    st = set(b)  
+    st = set(arr)
+    arr.sort() 
 
-    a = 0 
-    b = 0 
-    for i in range(len(arr)): 
-        if i % 2 != 0: 
+    a = sum(arr[n:])  
+    b = sum(arr[:n])  
+
+    a_arr = arr[n:] 
+    b_arr = arr[:n] 
+
+    res = [] 
+    if a - b > 0 and a-b in st: 
+        for i in range(n - 1, -1, -1): 
+            a -= arr[i+n] 
             a += arr[i] 
-        else: 
-            b -= arr[i]  
-        
-    x = a + b 
-    i = 
-    if x not in st: 
-        print([x] + arr) 
-    
+
+            b -= arr[i] 
+            b += arr[i+n] 
+
+            print(a-b) 
+
+            if a - b > 0 and a - b not in st: 
+                a_arr = arr[i:i+n] 
+                b_arr = arr[:i] + arr[i+n:] 
+                break 
+
+    res.append(a-b) 
+    for i in range(n): 
+        res.append(a_arr[i]) 
+        res.append(b_arr[i])  
+
+    print(*res) 
 
         
-    
-            
